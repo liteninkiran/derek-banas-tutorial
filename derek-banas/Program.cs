ce9368845d7606d543f27f580d1952a7b439676a
Kiran Anand <liteninkiran@hotmail.com>
2020-10-29 16:23:44 +0000

    Join strings and function call

--- derek-banas/Program.cs
+++ derek-banas/Program.cs
@@ -10,10 +10,24 @@ namespace derek_banas
 
             for(int i = 0; i <args.Length; i++)
             {
-                Console.WriteLine(args[i]);
+                Console.WriteLine("Arg {0}: {1}", i, args[i]);
             }
 
+            string[] myArgs = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(string.Join(", ", myArgs));
+
+            sayHello();
+
             Console.ReadLine();
+        }
+
+        private static void sayHello()
+        {
+            string name = "";
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
         }
     }
 }
