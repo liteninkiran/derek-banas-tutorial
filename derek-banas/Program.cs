322784623c5cd5b4d94a9b2124366280ddc4a0cb
Kiran Anand <liteninkiran@hotmail.com>
2020-10-29 16:37:02 +0000

    Data Types

--- derek-banas/Program.cs
+++ derek-banas/Program.cs
@@ -6,18 +6,20 @@ namespace derek_banas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
             for(int i = 0; i <args.Length; i++)
             {
-                Console.WriteLine("Arg {0}: {1}", i, args[i]);
+                //Console.WriteLine("Arg {0}: {1}", i, args[i]);
             }
 
             string[] myArgs = Environment.GetCommandLineArgs();
 
-            Console.WriteLine(string.Join(", ", myArgs));
+            //Console.WriteLine(string.Join(", ", myArgs));
 
-            sayHello();
+            //sayHello();
+
+            dataTypes();
 
             Console.ReadLine();
         }
@@ -28,6 +30,30 @@ namespace derek_banas
             Console.Write("Enter your name: ");
             name = Console.ReadLine();
             Console.WriteLine("Hello {0}", name);
+        }
+
+        private static void dataTypes()
+        {
+            decimal decPi  = 3.1415926535897932384626433832M;
+            decimal decNum = 0.0000000000000000000000000018M;
+
+            double dblPi  = 3.141592653589793;
+            double dblNum = 0.0000000000000004;
+
+            Console.WriteLine("Smallest Long: {0}", long.MinValue);
+            Console.WriteLine("Biggest Long: {0}", long.MaxValue);
+            Console.WriteLine("");
+
+            Console.WriteLine("DECIMAL Pi + Num: {0}", decPi + decNum);
+            Console.WriteLine("Smallest Decimal: {0}", decimal.MinValue);
+            Console.WriteLine("Biggest Decimal: {0}", decimal.MaxValue);
+            Console.WriteLine("");
+
+            Console.WriteLine("DOUBLE Pi + Num: {0}", dblPi + dblNum);
+            Console.WriteLine("Smallest Double: {0}", double.MinValue);
+            Console.WriteLine("Biggest Double: {0}", double.MaxValue);
+            Console.WriteLine("");
+
         }
     }
 }
